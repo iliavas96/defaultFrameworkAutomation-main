c3ace31c3b71b299ea5110e72bc078da695a4133
Ilia Vasilenko <iliavas96@yandex.ru>
2024-03-04 12:08:28 +0300

    промежуточный

--- src/test/java/pages/orderFeed/OrderFeedPage.java
+++ src/test/java/pages/orderFeed/OrderFeedPage.java
@@ -9,15 +9,8 @@ public class OrderFeedPage {
     private final By AREA_ORDER_ID = By.xpath(
             "//ul[@class='feed-info_list__1JWvG']/li[@class='feed-info_list_item__yrN7o']"); //находит  веб элемент, содержащий список id заков
 
-    private final String AREA_ID = ".feed-info_list_item__yrN7o"; // css селектор того же элемента. Я их просто еще не учил, сделал пока так
-
-    public String get_All_ID() { // возвращает css селектор
-        return AREA_ID;
-    }
-
     public SelenideElement getArea_Id() {
         return element(AREA_ORDER_ID); //возвращает element
     }
-
 
 }
--- src/test/java/test/creatBurger/ConstructorBurger.java
+++ src/test/java/test/creatBurger/ConstructorBurger.java
@@ -14,46 +14,42 @@ import static org.testng.Assert.assertEquals;
 
 public class ConstructorBurger {
     final private ConstructorPage constructorPage = new ConstructorPage();
-    final private ConstructorService constructorService = new ConstructorService();
-    private final AuthorizationService authorizationService = new AuthorizationService(); //создает объект класса AuthorizationServise
-    private final AuthorizationPage authorizationPage = new AuthorizationPage(); // создает объект класса AuthorizationPage
-    private final Authorization authorization = new Authorization();
 
 
-    void dragIngredient () { //добаввление ингрилиентов в конструктор бургеров
+    void dragIngredient () { //добаввление ингридиентов в конструктор бургеров
         for (int i = 0; i < constructorPage
-                .listIngredient_One().size(); i++)  //проверяет размер, не больше размера ArrayList ListIngridient_One
+                .listIngredient_One().size(); i++)
         {
             $(constructorPage
-                    .getListIngredient_One(i)) //выводит элемент ArrayList по индексу
+                    .getListIngredient_One(i))
                     .dragAndDropTo(
-                            constructorPage.getFilling_Addition_Area()); //переносит элементы ArrayList  List_Ingredient_One в область добавления ингридиентов)
+                            constructorPage.getFilling_Addition_Area());
         }
     }
 
       void compareIngredient() { // метод проверяет добавились ли ингрилиенты в конструктор
         for (int i = 0; i < constructorPage
-                .setIngredient_One().size(); i++) { //проверяет размер, не больше размера ArrayList setIngridient_One
+                .setIngredient_One().size(); i++) {
                     $(constructorPage
                         .getSetIngredient_One(i))
-                        .shouldBe(visible); // проверяет наличия элемента на странице
+                        .shouldBe(visible);
         }
     }
 
-      void comparePrice () { //сравнение суммы цен отдельных ингридиентов и общей цены показанной на сайте
-        int sumPrice = 0; //для суммы всех ингдредиентов
+    void comparePrice() { //сравнение суммы цен отдельных ингридиентов и общей цены показанной на сайте
+        int sumPrice = 0;
         for (int i = 0;
-             i < constructorPage.priceIngredient_One().size(); i++) {// i меньше size ArrayList PRICE_INGREDIENT_ONE
-            sumPrice += Integer.parseInt(                            // Складывает общую стоимость. Переводит String в INTEGER и потом в int для дальнейшего сравнения
+             i < constructorPage.priceIngredient_One().size(); i++) {
+            sumPrice += Integer.parseInt(
                     constructorPage
-                            .getPriceIngredient_One(i)         //метод возвращает элемент ArrayList PriceIngridient_One
-                            .getText());                             //Читает текст
+                            .getPriceIngredient_One(i)
+                            .getText());
 
         }
-            assertEquals(sumPrice,                                //Сравнивает два int - равна ли цена кажого отдельного эллемента - общей стоимости показанно на сайте
-                    Integer.parseInt(                               // Переводит String в INTEGER
-                            constructorPage
-                                    .getTotalPrice()                // Находит элемент TOTAL_PRICE
-                                    .getText()));                   // Читает его text.
+        assertEquals(sumPrice,
+                Integer.parseInt(
+                        constructorPage
+                                .getTotalPrice()
+                                .getText()));
         }
 }
